768efa1a774b701f9671052ee8a905f7f4093f17
VamsiPullamsetty <vamsipullamsetty@gmail.com>
2024-08-02 11:42:09 +0530

    build

---
+++ react-api-project-v1/build/precache-manifest.e61b13dd7d86fda4fafca12bd2717d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9652d4acaaf91d867262374004df6d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "249c5062ed5e37f31344",
+    "url": "/static/css/main.5937aba9.chunk.css"
+  },
+  {
+    "revision": "b840d1f2ffa65bb059e1",
+    "url": "/static/js/2.7f654837.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7f654837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249c5062ed5e37f31344",
+    "url": "/static/js/main.bdcb31b0.chunk.js"
+  },
+  {
+    "revision": "866ed326f9b633faeba8",
+    "url": "/static/js/runtime-main.0d63bdcd.js"
+  }
+]);
